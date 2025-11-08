4e1c3021b9410dd4487206793595d7716dd50e68
Sidharth <136654152+sidhu2690@users.noreply.github.com>
2025-11-08 00:06:46 -0800

    Update layerwise_landau_fit.C

--- toy_geometry/electron/layerwise_landau_fit.C
+++ toy_geometry/electron/layerwise_landau_fit.C
@@ -85,8 +85,8 @@ void layerwise_landau_fit() {
     for (Int_t i = 0; i < nValidLayers; i++) {
         Int_t iLayer = validLayers[i];
         layerArray[i] = iLayer;
-        filteredHitsArray[i] = filteredHitsPerLayer[iLayer];
-        filteredEnergyArray[i] = filteredTotalEnergyPerLayer[iLayer];
+        filteredHitsArray[i] = filteredHitsPerLayer[iLayer] / 100;
+        filteredEnergyArray[i] = filteredTotalEnergyPerLayer[iLayer] / 100;
     }
     
     std::ofstream outFile("energy_vs_layer.csv");
@@ -128,7 +128,7 @@ void layerwise_landau_fit() {
     gPad->SetGrid();
     
     TGraph *grEnergy = new TGraph(nValidLayers, layerArray, filteredEnergyArray);
-    grEnergy->SetTitle("Total Energy Deposited per Layer;Layer Number;Total Energy Deposited (MeV)");
+    grEnergy->SetTitle("Total Energy Deposited per Layer / 100;Layer Number;Total Energy Deposited (MeV) / 100");
     grEnergy->SetMarkerStyle(21);
     grEnergy->SetMarkerSize(1.2);
     grEnergy->SetMarkerColor(kRed+1);
